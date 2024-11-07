abc54465eeab8ceadafc0187068b9ff6e693f1c5
Jean-Baptiste DAULAUS <jeanbaptiste.daulaus@mh-core.com>
2024-11-07 17:58:13 +0100

    màj

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function addPawnToGrid(grid, pawn, x, y) {
-    // on vérifie qu'il n'y a pas déjà un pion à cette case
     if (!grid[y][x]) {
         grid[y][x] = pawn;
         return "";
@@ -7,59 +6,41 @@ function addPawnToGrid(grid, pawn, x, y) {
     return "Vous ne pouvez pas placer de pion ici";
 }
 
-module.exports = {addPawnToGrid};
+function displayGrid(grid) {
+    let result = "";
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            result += (x !== grid[y].length - 1)
+                ? grid[y][x] + " | "
+                : grid[y][x] + "\n";
+        }
+    }
+    return result;
+}
 
-// function playMove(grid, pawn) {
+function getWinner(grid, pawns) {
+    // on vérifie si y a un gagnant et dans ce cas là on le renvoie
+    for (let i = 0; i < pawns.length; i++) {
+        const pawn = pawns[i];
+        // verif horizontale
+        if (grid[0][0] === pawn && grid[0][1] === pawn && grid[0][2] === pawn) return pawn;
+        if (grid[1][0] === pawn && grid[1][1] === pawn && grid[1][2] === pawn) return pawn;
+        if (grid[2][0] === pawn && grid[2][1] === pawn && grid[2][2] === pawn) return pawn;
 
-// }
+        // verif verticale
+        if (grid[0][0] === pawn && grid[1][0] === pawn && grid[2][0] === pawn) return pawn;
+        if (grid[0][1] === pawn && grid[1][1] === pawn && grid[2][1] === pawn) return pawn;
+        if (grid[0][2] === pawn && grid[1][2] === pawn && grid[2][2] === pawn) return pawn;
 
-// function displayGrid(grid) {
-//     for (rows in grid) {
-//         for (let i = 0; i < rows.length; i++) {
-//             console.log(i != rows.length ? rows[i] + " |" : rows[i] + "\n");
-//         }
-//     }
-// }
+        // verif diagonale
+        if (grid[0][0] === pawn && grid[1][1] === pawn && grid[2][2] === pawn) return pawn;
+        if (grid[2][0] === pawn && grid[1][1] === pawn && grid[0][2] === pawn) return pawn;
+    }
+    return "";
+}
 
-// function checkIfPawnPresent(grid, x, y, pawn) {
-//     return grid[x][y] === pawn;
-// }
-
-// function gridChecker(grid, x, y) {
-//     for (let x = 0; x < grid.length; x++) {
-//         for (let y = 0; y < x.length; y++) {
-
-//         }
-//     }
-// }
-
-// function getWinner(grid, pawns) {
-//     // on vérifie si y a un gagnant et dans ce cas là on le renvoie
-//     for (let pawn in pawns) {
-//         // verif horizontale
-//         if (grid[0][0] === pawn && grid[1][0] === pawn && grid[2][0] === pawn) return pawn;
-//         if (grid[0][1] === pawn && grid[1][1] === pawn && grid[2][1] === pawn) return pawn;
-//         if (grid[0][2] === pawn && grid[1][2] === pawn && grid[2][2] === pawn) return pawn;
-
-//         // verif verticale
-//         if (grid[0][0] === pawn && grid[0][1] === pawn && grid[0][2] === pawn) return pawn;
-//         if (grid[1][0] === pawn && grid[1][1] === pawn && grid[1][2] === pawn) return pawn;
-//         if (grid[2][0] === pawn && grid[2][1] === pawn && grid[2][2] === pawn) return pawn;
-
-//         // verif diagonale
-//         if (grid[0][0] === pawn && grid[1][1] === pawn && grid[2][2] === pawn) return pawn;
-//         if (grid[0][2] === pawn && grid[1][1] === pawn && grid[0][0] === pawn) return pawn;
-//     }
-// }
-
-// function main() {
-//     let grid;
-//     let pawns = ["x", "o"];
-//     let isFirstPlayer = true;
-//     let isWinner = false;
-
-//     while(isWinner) {
-        
-//     }
-
-// }
+module.exports = {
+    addPawnToGrid,
+    displayGrid,
+    getWinner
+};
--- index.test.js
+++ index.test.js
@@ -1,19 +1,59 @@
-const { addPawnToGrid } = require(".");
+const {
+    addPawnToGrid,
+    displayGrid,
+    getWinner
+} = require(".");
 
+/* ADD PLAYER */
 test('should add a player pawn to the grid', () => {
-    let grid = [["", "", ""],["", "", ""],["", "", ""]];
+    let grid = [["", "", ""], ["", "", ""], ["", "", ""]];
     let pawn = "x";
     let x = 0;
     let y = 0;
     expect(addPawnToGrid(grid, pawn, x, y)).toBe("");
-    expect(grid).toStrictEqual([["x", "", ""],["", "", ""],["", "", ""]])
+    expect(grid).toStrictEqual([["x", "", ""], ["", "", ""], ["", "", ""]])
 });
 
-test('should return "Vous ne pouvez pas placer de pion ici" and not add pawn to the list', () => {
-    let grid = [["y", "", ""],["", "", ""],["", "", ""]];
+test('should return Vous ne pouvez pas placer de pion ici and not add pawn to the list', () => {
+    let grid = [["y", "", ""], ["", "", ""], ["", "", ""]];
     let pawn = "x";
     let x = 0;
     let y = 0;
     expect(addPawnToGrid(grid, pawn, x, y)).toBe("Vous ne pouvez pas placer de pion ici");
-    expect(grid).toStrictEqual([["y", "", ""],["", "", ""],["", "", ""]])
+    expect(grid).toStrictEqual([["y", "", ""], ["", "", ""], ["", "", ""]])
 });
+
+/* DISPLAY GRID */
+test('should display grid', () => {
+    let grid = [["y", "x", "x"], ["y", "x", "y"], ["x", "y", "x"]];
+    expect(displayGrid(grid)).toBe("y | x | x\ny | x | y\nx | y | x\n");
+});
+
+test('should display grid version 2', () => {
+    let grid = [["o", "o", "o"], ["x", "x", "x"], ["o", "o", "o"]];
+    expect(displayGrid(grid)).toBe("o | o | o\nx | x | x\no | o | o\n");
+});
+
+/* GET WINNER */
+test('should return o as winner', () => {
+    let grid = [["o", "o", "o"], ["x", "o", "o"], ["x", "x", "o"]];
+    let pawns = ["o", "x"];
+    expect(getWinner(grid, pawns)).toBe("o");
+});
+
+test('should return x as winner', () => {
+    let grid = [["x", "x", "x"], ["x", "o", "x"], ["x", "o", "x"]];
+    let pawns = ["o", "x"];
+    expect(getWinner(grid, pawns)).toBe("x");
+});
+
+test('should return no winner', () => {
+    let grid = [
+        ["x", "o", "x"],
+        ["o", "x", "o"],
+        ["o", "x", "o"]
+    ];
+    let pawns = ["o", "x"];
+    expect(getWinner(grid, pawns)).toBe("");
+});
+
